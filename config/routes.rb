4ce1fdecf95f15e3e6f9cbf3c2021bc4c1a28cbb
cerealCode <lfv@betternow.org>
2016-04-08 19:06:24 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  resources :users
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/about'
+
+  get 'static_pages/users'
+
+  get 'users'
+
+  
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
